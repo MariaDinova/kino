4c9441a3e54da0b08f7b9a91b0acf25b4a522065
Daniela-Toncheva <dani_398@abv.bg>
2019-02-21 15:48:44 +0200

    test login

--- users/view/login.php
+++ users/view/login.php
@@ -1,2 +1,18 @@
 <?php
 // TODO Make form with fields for e-mail, password and submit. ?page=users&action=login
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+
+</body>
+</html>
