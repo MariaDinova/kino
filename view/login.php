ae030c22a8075ef73f39823794ff0f56c5b39738
Daniela-Toncheva <dani_398@abv.bg>
2019-02-24 14:57:03 +0200

    made login form

--- view/login.php
+++ view/login.php
@@ -1,8 +1,6 @@
 <?php
-// TODO Make form with fields for e-mail, password and submit. ?page=users&action=login
 
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,9 +8,29 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title> Please Login</title>
 </head>
 <body>
-
+<form action="<?php BASE_PATH ?>?target=user&action=login" method="post">
+    <table>
+        <tr>
+            <td>Email</td>
+            <td>
+                <input type="email" name="email" placeholder="your email">
+            </td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td>
+                <input type="password" name="password" placeholder="your password">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" name="login" value="Log in">
+            </td>
+        </tr>
+    </table>
+</form>
 </body>
 </html>
